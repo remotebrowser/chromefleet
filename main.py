7e4cad0caccb4adb7c82112e9f116a0aeb9b9381
Ariya Hidayat <ariya@getgather.com>
2025-12-18 21:58:27 -0800

    /health with FastAPI

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
-print("Hello, World!")
+import uvicorn
+
+from datetime import datetime
+from fastapi import FastAPI
+
+app = FastAPI(title="Chrome Fleet")
+
+
+@app.get("/health")
+async def health() -> str:
+    return f"OK {int(datetime.now().timestamp())}"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8300, reload=True)
